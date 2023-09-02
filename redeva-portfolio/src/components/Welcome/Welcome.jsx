62623c30f4d39458ae09acf05532e18a9f3e34df
evaredon <121177767+evaredon@users.noreply.github.com>
2023-09-03 00:59:34 +0200

    landing page fix design

--- redeva-portfolio/src/components/Welcome/Welcome.jsx
+++ redeva-portfolio/src/components/Welcome/Welcome.jsx
@@ -1,6 +1,5 @@
 import Profile from '../../assets/welcome-pic1.png'
 import Location from '../../assets/location.svg'
-import Cloud from '../../assets/cloud.png'
 
 import './Welcome.scss'
 
@@ -8,10 +7,14 @@ function Welcome() {
 return(
     <div className='Welcome'>
 
+        <div className="custom-shape-divider-top-1693694955">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
+        </div>
+
         <div className='Welcome-Message'>
-            <img className='Welcome-Cloud' src={Cloud} />
-            <h1>Bonjour</h1>
-            <img className='Welcome-Cloud' src={Cloud} />
+            <h1>Bonjour !</h1>
         </div>
 
         <div className='Welcome-Top'>
