7b5f1ecbb5257fae09f15919cb9e229bb25707ac
evaredon <121177767+evaredon@users.noreply.github.com>
2023-09-27 12:23:47 +0200

    get in touch part design ok

--- redeva-portfolio/src/components/Contact/Intro/Intro.jsx
+++ redeva-portfolio/src/components/Contact/Intro/Intro.jsx
@@ -1,13 +1,25 @@
+import { Link } from 'react-router-dom';
 import './Intro.scss';
 
 function Intro() {
   return (
-        <div className='Contact-Info'>
-            <section>
-            <p><span className='Contact-Join'>Email : </span>evaredon2001@gmail.com</p>
-            <p><span className='Contact-Join'>Phone :</span>+33 6 77 29 88 62</p>
-            </section>
-        </div>
+    <div className="Contact-Info">
+      <h3>Get in touch</h3>
+      <div>
+        <section className='Contact-Section'>
+          <h4>Mail Adress</h4>
+          <p>evaredon2001@gmail.com</p>
+        </section>
+        <section className='Contact-Section'>
+          <h4>Phone Number</h4>
+          <p>+33 6 77 29 88 62</p>
+        </section>
+        <section className='Contact-Section'>
+          <h4>Github Link</h4>
+          <Link to="https://github.com/evaredon">github.com/evaredon</Link>
+        </section>
+      </div>
+    </div>
   );
 }
 
