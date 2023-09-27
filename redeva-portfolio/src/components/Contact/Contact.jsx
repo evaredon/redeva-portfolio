830ebd1b457589e5461909eb1ce26615d7b05303
evaredon <121177767+evaredon@users.noreply.github.com>
2023-09-27 12:35:25 +0200

    need to dynamise everything now

--- redeva-portfolio/src/components/Contact/Contact.jsx
+++ redeva-portfolio/src/components/Contact/Contact.jsx
@@ -1,12 +1,10 @@
 import './Contact.scss';
-// import Letter from '../../assets/loveletter.png'
 import Form from './Form/Form';
 import Intro from './Intro/Intro';
 
 function Contact() {
   return (
     <div className="Contact">
-      {/* <img className="Contact-Letter" src={Letter} alt="Love letter drawing" /> */}
       <div className='Contact-FormSide'>
         <h2>Contact</h2>
         <p>If you wanna learn more about me, talk about a project collaboration or just say hi, you can send me an email !</p>
--- redeva-portfolio/src/components/Contact/Intro/Intro.jsx
+++ redeva-portfolio/src/components/Contact/Intro/Intro.jsx
@@ -1,20 +1,21 @@
 import { Link } from 'react-router-dom';
 import './Intro.scss';
+import Letter from '../../../assets/loveletter.png';
 
 function Intro() {
   return (
     <div className="Contact-Info">
-      <h3>Get in touch</h3>
+      <img className="Contact-Letter" src={Letter} alt="Love letter drawing" />
       <div>
-        <section className='Contact-Section'>
+        <section className="Contact-Section">
           <h4>Mail Adress</h4>
           <p>evaredon2001@gmail.com</p>
         </section>
-        <section className='Contact-Section'>
+        <section className="Contact-Section">
           <h4>Phone Number</h4>
           <p>+33 6 77 29 88 62</p>
         </section>
-        <section className='Contact-Section'>
+        <section className="Contact-Section">
           <h4>Github Link</h4>
           <Link to="https://github.com/evaredon">github.com/evaredon</Link>
         </section>
