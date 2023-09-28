ffab90f5224da5f0630ff48095e5a7e682c24a13
evaredon <121177767+evaredon@users.noreply.github.com>
2023-09-28 10:48:53 +0200

    contact form functionnality ok

--- redeva-portfolio/src/components/Contact/Form/Form.jsx
+++ redeva-portfolio/src/components/Contact/Form/Form.jsx
@@ -1,18 +1,60 @@
-import './Form.scss'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './Form.scss';
 
 function Form() {
-    return (
-        <div className='Contact-Form'>
-        <form>
-          <label>Email</label>
-          <input type="text" placeholder="Enter your Email" className="Contact-Mail" />
-          <label>Message</label>
-          <input placeholder="Write your message here" className="Contact-Message" />
-          <button>Send Message</button>
-        </form>
-        </div>
-    );
-  }
-  
-  export default Form;
-  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_w4p097a',
+        'template_v9bctav',
+        form.current,
+        'K1yQsgtk9w3BNHmKv'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="Contact-Form">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Enter your name"
+          className="Contact-Mail"
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Enter your Email"
+          className="Contact-Mail"
+        />
+        <label>Message</label>
+        <textarea
+          name="message"
+          placeholder="Write your message here"
+          className="Contact-Message"
+        />
+        <button type="submit" value="Send">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
